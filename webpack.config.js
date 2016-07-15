d24656c63c36eae2fe0e7572ffc0bd952b34dee6
VSKor <vs.korolenko@gmail.com>
2016-07-15 14:26:21 +0300

    configure webpack: - hot reload enabled. - split on dev and prod builds.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,13 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
+var DEV = false;
+process.argv.forEach(function (value) {
+  if (value.indexOf('webpack-dev-server') >= 0) {
+    DEV = true;
+  }
+});
+
 var config = {
   entry: APP_DIR + '/app.jsx',
   output: {
@@ -28,11 +35,39 @@ var config = {
         loaders: ['style', 'css', 'sass-loader']
       }
     ]
-  },
-
-  devtool: "sourcemap",
-
-  watch: true
+  }
 };
+
+if (DEV) {
+  config.entry = [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    APP_DIR + '/app.jsx'
+  ];
+
+  config.module.loaders.unshift({
+    test: /\.jsx?/,
+    include: APP_DIR,
+    loader: 'react-hot'
+  });
+
+  config.devtool = "sourcemap";
+
+  config.watch = true;
+
+  config.devServer = {
+    contentBase: "./public",
+    noInfo: true,
+    hot: true,
+    inline: true,
+    port: '3000',
+    host: '0.0.0.0'
+  };
+
+  config.plugins = [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ];
+}
 
 module.exports = config;
