ca61512b0c5d46f9f76cb7ca7e1cf58dcdecdbe6
VSKor <vs.korolenko@gmail.com>
2016-07-19 13:12:12 +0300

    DEV webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,13 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
-var DEV = false;
-process.argv.forEach(function (value) {
-  if (value.indexOf('webpack-dev-server') >= 0) {
-    DEV = true;
-  }
-});
-
 var config = {
-  entry: APP_DIR + '/app.jsx',
+  entry: [
+    'webpack-dev-server/client?http://0.0.0.0:3000',
+    'webpack/hot/only-dev-server',
+    APP_DIR + '/app.jsx'
+  ],
+
   output: {
     path: BUILD_DIR,
     publicPath: '/',
@@ -21,6 +19,11 @@ var config = {
 
   module: {
     loaders: [
+      {
+        test: /\.jsx?/,
+        include: APP_DIR,
+        loader: 'react-hot'
+      },
       {
         test: /\.jsx?/,
         include: APP_DIR,
@@ -39,7 +42,7 @@ var config = {
         test: /\.(jpg|jpeg|gif|png)$/,
         include: APP_DIR,
         exclude: /node_modules/,
-        loader:'url-loader?limit=1024&name=images/[name].[ext]'
+        loader: 'url-loader?limit=1024&name=images/[name].[ext]'
       },
       {
         test: /\.(woff|woff2|eot|ttf|svg)$/,
@@ -50,51 +53,23 @@ var config = {
     ]
   },
 
-  plugins: [new webpack.optimize.UglifyJsPlugin({
-    exclude: /\.min\.js?/
-  })],
-  
-  resolve: {
-    extensions: ['', '.js', '.jsx'],
-    alias: {
-      "react": path.resolve(__dirname, 'node_modules/react/dist/react.min.js'),
-      "react-dom": path.resolve(__dirname, 'node_modules/react-dom/dist/react-dom.min.js')
+  plugins: [
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  devtool: "sourcemap",
+
+  devServer: {
+    contentBase: "./public",
+    hot: true,
+    port: '3000',
+    host: '0.0.0.0',
+    stats: {
+      chunks: false,
+      colors: true
     }
   }
 };
-
-if (DEV) {
-  config.entry = [
-    'webpack-dev-server/client?http://0.0.0.0:3000',
-    'webpack/hot/only-dev-server',
-    APP_DIR + '/app.jsx'
-  ];
-
-  config.module.loaders.unshift({
-    test: /\.jsx?/,
-    include: APP_DIR,
-    loader: 'react-hot'
-  });
-
-  config.devtool = "sourcemap";
-
-  config.watch = true;
-
-  config.devServer = {
-    contentBase: "./public",
-    noInfo: true,
-    hot: true,
-    inline: true,
-    port: '3000',
-    host: '0.0.0.0'
-  };
-
-  config.plugins = [
-    new webpack.NoErrorsPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ];
-
-  delete config.resolve.alias;
-}
 
 module.exports = config;
